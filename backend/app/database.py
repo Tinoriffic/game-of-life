5a512ccb7864f002a0636008865aec53dfd1c14d
Faustino <faustinomandrade@gmail.com>
2023-11-19 22:26:16 -0500

    replace deprecated module

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql://faustino:password@localhost/game_of_life"
 
