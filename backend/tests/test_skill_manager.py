32d84da90f8de299d1c1f6de565980f27f74accb
Faustino <faustinomandrade@gmail.com>
2023-11-19 22:27:39 -0500

    fix test_skill_manager + additional tests in xp_calculator

--- backend/tests/test_skill_manager.py
+++ backend/tests/test_skill_manager.py
@@ -1,39 +1,37 @@
 import pytest
-from unittest.mock import Mock, patch
+from unittest.mock import patch, create_autospec
+from sqlalchemy.orm import Session
 from app.skill_manager import update_skill_xp, calculate_required_xp
 from app.models import Skill
 
-# Mock the Skill model and database session
-class MockSkill(Skill):
-    def __init__(self, name, xp, level, daily_xp_earned):
-        self.name = name
-        self.xp = xp
-        self.level = level
-        self.daily_xp_earned = daily_xp_earned
-
-@pytest.fixture
-def mock_db_session():
-    # Mock session class
-    class MockSession:
-        def query(self, model):
-            return self
-        def filter(self, *args, **kwargs):
-            return self
-        def first(self):
-            return MockSkill("TestSkill", 100, 1, 0)
-        def commit(self):
-            pass
-
-    return MockSession()
-
-# Test calculate_required_xp function
 def test_calculate_required_xp():
     assert calculate_required_xp(1) == 100  # Basic case
     assert calculate_required_xp(2) == int(100 * (2 ** 1.5))  # Next level
 
+# Mock setup for Skill model and Session
+@pytest.fixture
+def mock_skill():
+    return Skill(name="TestSkill", xp=50, level=1, daily_xp_earned=0)
+
+@pytest.fixture
+def mock_session(mock_skill):
+    session = create_autospec(Session, instance=True)
+    session.query.return_value.filter.return_value.first.return_value = mock_skill
+    return session
+
 # Test update_skill_xp function
-def test_update_skill_xp(mock_db_session):
-    with patch('your_project.skill_manager.db', mock_db_session):
-        skill = update_skill_xp(mock_db_session, 1, "TestSkill", 50)
-        assert skill.xp == 150  # New total XP
-        assert skill.daily_xp_earned == 50  # XP earned for the day
+def test_update_skill_xp(mock_session):
+    with patch('app.skill_manager.Session', return_value=mock_session):
+        skill = mock_session.query(Skill).filter().first()
+        assert skill.level == 1
+
+        update_skill_xp(mock_session, 1, "TestSkill", 50)
+        skill = mock_session.query(Skill).filter().first()
+        assert skill.level == 2
+        assert skill.xp == 0  # New total XP should be 0 since 100 XP is a level up (at level 1)
+        assert skill.daily_xp_earned == 50
+        
+        update_skill_xp(mock_session, 1, "TestSkill", 20)
+        skill = mock_session.query(Skill).filter().first()
+        assert skill.xp == 20
+        assert skill.level == 2
--- backend/tests/test_xp_calculator.py
+++ backend/tests/test_xp_calculator.py
@@ -2,6 +2,7 @@ from app.xp_calculator import (
     calculate_meditation_xp, calculate_running_xp, calculate_social_interaction_xp,
     calculate_learning_xp, calculate_reflection_xp, calculate_weight_tracking_xp
 )
+from datetime import datetime, timedelta
 
 def test_calculate_meditation_xp():
     assert calculate_meditation_xp(15, 0) == 20  # First session
@@ -22,3 +23,34 @@ def test_calculate_learning_xp():
 def test_calculate_reflection_xp():
     assert calculate_reflection_xp(0) == 20  # First reflection
     assert calculate_reflection_xp(10) == 0  # Subsequent reflection
+
+def test_calculate_weight_tracking_xp():
+    daily_logs = [MockWeightLog(70, datetime(2023, 1, 1)), MockWeightLog(69, datetime(2023, 1, 2))]
+    assert calculate_weight_tracking_xp(daily_logs, 75, 65) == 4  # Only 4 XP is rewarded for logging 2 weight entries
+
+def test_calculate_weight_tracking_xp_progress():
+    # Setup: User's goal is to lose weight (e.g., from 70lb to 65lb)
+    starting_weight = 70
+    goal_weight = 65
+
+    # Create mock logs showing a progressive decrease in weight over two weeks
+    daily_logs = [
+        MockWeightLog(70, datetime.now() - timedelta(days=14)),  # Start of tracking
+        MockWeightLog(69.7, datetime.now() - timedelta(days=12)),
+        MockWeightLog(69.5, datetime.now() - timedelta(days=11)),
+        MockWeightLog(69, datetime.now() - timedelta(days=9)),
+        MockWeightLog(68.5, datetime.now() - timedelta(days=7)),   # Start of week 2
+        MockWeightLog(68, datetime.now() - timedelta(days=6)),
+        MockWeightLog(67.9, datetime.now() - timedelta(days=4)),
+        MockWeightLog(68, datetime.now() - timedelta(days=3)),
+        MockWeightLog(67.6, datetime.now() - timedelta(days=1)),   # Most recent entry
+    ]
+
+    # Call the function to calculate XP
+    xp_earned = calculate_weight_tracking_xp(daily_logs, starting_weight, goal_weight)
+    assert xp_earned >= 32  # (2 * weekly reward of 15 XP) + 2 XP for logging last entry
+
+class MockWeightLog:
+    def __init__(self, weight, date):
+        self.weight = weight
+        self.date = date
