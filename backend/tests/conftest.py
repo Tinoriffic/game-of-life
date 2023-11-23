b1dbeed587e7aa1cf3cc54a48707252685ec26f2
Faustino <faustinomandrade@gmail.com>
2023-11-22 20:19:40 -0500

    mock test user

---
+++ backend/tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+from app.models import User, Skill, ActivityStreak
+from datetime import date, timedelta
+
+@pytest.fixture(scope="module")
+def mock_user():
+    return User(
+        id=1,
+        username="testuser",
+        email="testuser@example.com",
+        first_name="XP",
+        last_name="Junkie",
+        city="Cyberspace",
+        occupation="Data Sorcerer"
+    )
+
+@pytest.fixture(scope="module")
+def mock_skills():
+    base_skills = ['Awareness', 'Charisma', 'Endurance', 'Intelligence', 'Strength', 'Wisdom']
+    skills = []
+    for skill in base_skills:
+        if skill == 'Awareness':
+            skills.append(Skill(name=skill, xp=40, level=0, daily_xp_earned=0))
+        else:
+            skills.append(Skill(name=skill, xp=0, level=1, daily_xp_earned=0))
+    return skills
+
+@pytest.fixture
+def mock_streaks():
+    yesterday = date.today() - timedelta(days=1)
+    two_days_ago = date.today() - timedelta(days=2)
+    return [
+        ActivityStreak(user_id=1, activity_type="meditate", current_streak=2, last_activity_date=two_days_ago),
+        ActivityStreak(user_id=1, activity_type="weight_tracking", current_streak=3, last_activity_date=yesterday),
+        # Add other streaks if necessary
+    ]
