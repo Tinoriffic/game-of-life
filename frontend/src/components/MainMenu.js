68b7cb4bdbfe7f6d9782464a1068757c8dc838b3
Faustino <faustinomandrade@gmail.com>
2024-02-28 21:58:50 -0500

    create a user context for storing user's id to be used in API requests

--- frontend/src/components/MainMenu.js
+++ frontend/src/components/MainMenu.js
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axiosInstance from '../axios';
 import './MainMenu.css'
 import PlayerCard from './player/PlayerCard';
 import { useNavigate } from 'react-router-dom';
 import { baseUrl } from '../config/apiConfig';
+import { useUser } from './player/UserContext';
 
 function MainMenu() {
   const token = localStorage.getItem('accessToken');
   const [error, setError] = useState('');
   const [playerData, setPlayerData] = useState(null);
   const navigate = useNavigate();
+  const { setUser } = useUser();
 
   console.log('MainMenu: Using access token:', token);
 
@@ -21,7 +23,10 @@ function MainMenu() {
             'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
           }
         });
+
         setPlayerData(response.data);
+        localStorage.setItem('userId', response.data.id.toString());
+        setUser({id: response.data.id});
       } catch (error) {
         if (error.response && error.response.status === 401) {
           setError(error.response.data.detail);
@@ -56,46 +61,5 @@ function MainMenu() {
     </div>
   );
 }
-
-// TODO: Adjust this get the current user's data and if unable to, some default settings
-const yourPlayerData = {
-  avatar: 'https://th.bing.com/th/id/OIG.EpsJXjL4FVpp8UnVU.OV?pid=ImgGn',
-  name: 'Faustino',
-  region: 'Boston',
-  occupation: 'Software Engineer',
-  stats: {
-    awareness: {
-      level: 2,
-      currXP: 30,
-      maxXP: 100
-    },
-    charisma: {
-      level: 1,
-      currXP: 50,
-      maxXP: 100
-    },
-    endurance: {
-      level: 1,
-      currXP: 92,
-      maxXP: 100
-    },
-    intelligence: {
-      level: 1,
-      currXP: 22,
-      maxXP: 100
-    },
-    strength: {
-      level: 1,
-      currXP: 70,
-      maxXP: 100
-    },
-    wisdom: {
-      level: 2,
-      currXP: 12,
-      maxXP: 100
-    }
-  }
-};
-
 
 export default MainMenu;
---
+++ frontend/src/components/player/UserContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const UserContext = createContext();
+
+export const useUser = () => useContext(UserContext);
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem('userId');
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
